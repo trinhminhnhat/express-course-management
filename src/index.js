2a142221041c5959ae54078984f990e1cbecc4c1
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-03-05 20:21:08 +0700

    + add node-sass package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
