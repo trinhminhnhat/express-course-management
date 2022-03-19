5edea31d33c0c7778384604db61f26282104d5a5
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-03-19 21:50:16 +0700

    + implement sort middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // handle middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(sortMiddleware);
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -32,6 +34,23 @@ app.engine(
 		extname: '.hbs',
 		helpers: {
 			sum: (a, b) => a + b,
+			sortable: (column, sort) => {
+				const sortType = sort.column === column ? sort.type : 'default';
+				const icons = {
+					default: 'fa-solid fa-sort',
+					asc: 'fa-solid fa-sort-down',
+					desc: 'fa-solid fa-sort-up',
+				};
+				const types = {
+					default: 'desc',
+					asc: 'desc',
+					desc: 'asc',
+				};
+
+				return `<a href="?_sort&column=${column}&type=${types[sortType]}">
+							<i class="${icons[sortType]}"></i>
+						</a>`;
+			},
 		},
 	})
 );
